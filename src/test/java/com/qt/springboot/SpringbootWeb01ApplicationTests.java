92a5e2399e1fe0efa50c65b2336979c381acfdf7
qing <qing@qq.com>
2022-05-16 16:33:37 +0800

    third commit

--- src/test/java/com/qt/springboot/SpringbootWeb01ApplicationTests.java
+++ src/test/java/com/qt/springboot/SpringbootWeb01ApplicationTests.java
@@ -9,6 +9,11 @@ class SpringbootWeb01ApplicationTests {
 @Test
     public void test(){
     System.out.println("hello git!");
+    System.out.println("hello git!");
+    System.out.println("hello git!");
+    System.out.println("hello git!");
+    System.out.println("hello git!");
+    System.out.println("hello git!");
 }
 
 }
